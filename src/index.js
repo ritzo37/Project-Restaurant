a0bee445c0a60b932f227916e35a47420558f006
ritzo37 <ritishrai56@gmail.com>
2024-07-17 03:10:56 +0530

    Initial Config

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log("Initial Configuration is working!");
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry : './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      template : '/src/index.html'
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer : {
+     static : './dist' ,
+     watchFiles : './src/index.html',
+  } , 
+  devtool: 'inline-source-map',
+  mode : 'development' ,
+};
